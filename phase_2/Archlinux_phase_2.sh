c2ff967293f96d3358ba0fc747331b83ba1e9d29
2ELCN0168 <eluciani@cpzardaigoro01o.home.arpa>
2025-01-13 23:04:55 +0100

    Started phase 2

---
+++ phase_2/Archlinux_phase_2.sh
@@ -0,0 +1,12 @@
+# source "./phase_2/functions/"
+source "./config/text_formatting.sh"
+source "./phase_2/functions/init_phase_2.sh"
+
+function main()
+{
+        init_phase_2
+
+        # partition_disks
+}
+
+main
---
+++ phase_2/functions/init_phase_2.sh
@@ -0,0 +1,5 @@
+function init_phase_2()
+{
+        printf "%b" "${C_G}This is the second phase of the script.${N_F} "
+        printf "%b" "${C_C}The installation starts now.${N_F}\n\n"
+}

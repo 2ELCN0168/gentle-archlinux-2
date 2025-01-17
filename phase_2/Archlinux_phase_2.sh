d500da816d13856e7ab7290a84a9acf9e7ee1b29
2ELCN0168 <eluciani@cpzardaigoro01o.home.arpa>
2025-01-17 15:23:19 +0100

    Corrected link

--- phase_2/Archlinux_phase_2.sh
+++ phase_2/Archlinux_phase_2.sh
@@ -2,6 +2,7 @@
 source "./config/text_formatting.sh"
 source "./phase_2/functions/init_phase_2.sh"
 source "./phase_2/functions/disks_fs/partition_disks.sh"
+source "./phase_2/functions/disks_fs/create_luks.sh"
 source "./phase_2/functions/disks_fs/create_lvm.sh"
 
 function main()

8888e6f51eea558a738db725b4e8bb7a549a30b6
2ELCN0168 <eluciani@cpzardaigoro01o.home.arpa>
2025-01-17 15:11:14 +0100

    Added lvm

--- phase_2/Archlinux_phase_2.sh
+++ phase_2/Archlinux_phase_2.sh
@@ -2,6 +2,7 @@
 source "./config/text_formatting.sh"
 source "./phase_2/functions/init_phase_2.sh"
 source "./phase_2/functions/disks_fs/partition_disks.sh"
+source "./phase_2/functions/disks_fs/create_lvm.sh"
 
 function main()
 {
@@ -10,6 +11,8 @@ function main()
         partition_disks
 
         create_luks
+
+        create_lvm
 }
 
 main
---
+++ phase_2/functions/disks_fs/create_lvm.sh
@@ -0,0 +1,28 @@
+function create_lvm()
+}
+        local disk_list
+        disk_list="$(jaq -r '.system.drives.selected_drives' ${json_config})"
+        local first_disk
+        first_disk="$(echo ${disk_list} | cut -d ' ' -f 1)"
+        disk_list="$(echo ${disk_list[@]} | cut -d ' ' -f 2-)"
+
+        declare -A volumes_list
+        
+        while IFS=$'\t' read -r mountpoint size; do
+                volumes["${mountpoint}"]="${size}"
+        done < <(cat ${json_config}) | 
+        jaq -r '.drives.volumes.volumes_list[] | "\(.mountpoint)\t\(.size)"'
+
+        pvcreate "/dev/${first_disk}"
+
+        
+        local pv_list
+        for i in ${disk_list[@]}; do
+                pvcreate "/dev/${i}"
+                pv_list+=("/dev/${i}")
+        done
+
+        vgcreate vg_archlinux ${pv_list[@]}
+
+        lvcreate -L 15G -n root vg_archlinux
+}

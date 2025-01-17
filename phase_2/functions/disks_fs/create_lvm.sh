ba4bef035a93fc8c15b13ece96862799939c5e36
2ELCN0168 <eluciani@cpzardaigoro01o.home.arpa>
2025-01-17 17:03:03 +0100

    Corrected LVM

--- phase_2/functions/disks_fs/create_lvm.sh
+++ phase_2/functions/disks_fs/create_lvm.sh
@@ -24,11 +24,32 @@ function create_lvm()
 
         vgcreate vg_archlinux ${pv_list[@]}
 
+        local has_efi=0
+        local has_boot=0
+        for i in "${!volumes_list[@]}"; do
+                if [[ "${i}" == "/efi" ]]; then
+                        has_efi=1
+                elif [[ "${i}" == "/boot" ]]; then
+                        has_boot=1
+                fi
+        done
+
         for i in "${!volumes_list[@]}"; do
                 name="$(echo ${!volumes_list[${i}]} | sed 's/\//-/g' | cut -c 2-)"
                 if [[ -z "${name}" ]]; then
                         name="root"
                 fi
+
+                # Skip efi volume if boot volume is also there.
+                if [[ "${has_efi}" -eq 1 && "${has_boot}" -eq 1 && "${name}" == "efi" ]]; then
+                        continue
+                fi
+
+                # Skip boot volume if there is no efi volume.
+                if [[ "${has_efi}" -eq 0 && "${has_boot}" -eq 1 && "${name}" == "boot" ]]; then
+                        continue
+                fi
+
                 lvcreate -L "${volumes_list[${i}]}" -n "${name}" vg_archlinux
         done
 }

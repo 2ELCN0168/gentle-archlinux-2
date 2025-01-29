c44648f33054441b6f7e3f0d229b7573128fa1a9
2ELCN0168 <eluciani@cpzardaigoro01o.home.arpa>
2025-01-29 21:13:10 +0100

    Corrected typo: drive's'

--- phase_1/functions/configure_mkinitcpio.sh
+++ phase_1/functions/configure_mkinitcpio.sh
@@ -5,9 +5,9 @@ function configure_mkinitcpio()
 
         local _btrfs _luks _lvm
 
-        _btrfs="$(jaq -r '.drives.filesystem' ${json_config})"
-        _luks="$(jaq -r '.drives.encryption' ${json_config})"
-        _lvm="$(jaq -r '.drives.lvm' ${json_config})"
+        _btrfs="$(jaq -r '.drive.filesystem' "${json_config}")"
+        _luks="$(jaq -r '.drive.encryption' "${json_config}")"
+        _lvm="$(jaq -r '.drive.lvm' "${json_config}")"
 
         # The filesystem must be btrfs only (just for the hook)
         if [[ "${_btrfs}" == "btrfs" ]]; then
@@ -33,6 +33,7 @@ function configure_mkinitcpio()
         hooks="base systemd ${_btrfs}autodetect modconf kms keyboard sd-vconsole ${_encryption}block ${_lvm}filesystems fsck"
 
         jaq -i '.system.initcpio_hooks = "'"${hooks}"'"' "${json_config}"
-        
+
         printf "%b" "${INFO} Changed initcpio hooks to: ${C_P}${hooks}${N_F}.\n\n"
 }
+

7e0e5235d87f2f32dbe2a27f64816d84562990a3
2ELCN0168 <eluciani@cpzardaigoro01o.home.arpa>
2025-01-29 21:20:41 +0100

    Corrected second stage beginning

--- phase_2/functions/init_phase_2.sh
+++ phase_2/functions/init_phase_2.sh
@@ -1,20 +1,23 @@
 function init_phase_2()
 {
-        printf "%b" "${C_G}This is the second phase of the script.${N_F} "
+        printf "%b" "${C_C}: :: : : ${C_G}STAGE 2 COMING IN. "
+        printf "%b" "${C_C}: :: : :${N}\n"
+
         printf "%b" "${C_C}The installation starts now.${N_F}\n\n"
 
-        printf "%b" "${C_Y}Type Ctrl + C to stop the installation.\n"
-        printf "%b" "${C_R}Installation will start in: "
+        local ans
 
-        for i in {10..0}; do
-                printf "%b" "${C_Y}${i}${N_F}"
-                if [[ "${i}" -eq 0 ]]; then
-                        break
-                fi
-                sleep 0.33
-                for a in {1..2}; do
-                        printf "%b" "."
-                        sleep 0.33
-                done
+        while true; do
+                printf "%b" "${C_R}Do you want to begin the installation? "
+                printf "%b" "[Y/n] ->${N} "
+
+                read -r ans
+                : "${ans:=Y}"
+
+                case "${ans}" in
+                [yY]) break ;;
+                [nN]) exit 0 ;;
+                *) invalid_answer ;;
+                esac
         done
 }

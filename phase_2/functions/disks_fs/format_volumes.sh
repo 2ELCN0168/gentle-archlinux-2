65cf2de49f648090e16cf07af84c9da2c1adb2c4
2ELCN0168 <eluciani@cpzardaigoro01o.home.arpa>
2025-02-04 14:04:41 +0100

    Added format_lvm function

--- phase_2/functions/disks_fs/format_volumes.sh
+++ phase_2/functions/disks_fs/format_volumes.sh
@@ -12,7 +12,7 @@ function format_volumes()
         while IFS=$'\t' read -r mountpoint size; do
                 volumes["${mountpoint}"]="${size}"
         done < <(jaq -r \
-                '.drive.volumes.volumes_list[] | "\(mountpoint)\t\(.size)"' \
+                '.drive.volumes.volumes_list[] | "\(.mountpoint)\t\(.size)"' \
                 "${json_config}")
 
         if [[ "${is_lvm}" -eq 0 ]]; then
@@ -43,10 +43,45 @@ function format_no_lvm()
                 fi
 
                 if ! mkfs."${filesystem}" "/dev/${disk}${counter}" \
-                        1> "/dev/null"; then
+                        1> "/dev/null" 2>&1; then
                         break
                 else
                         ((counter++))
                 fi
         done
+}
+
+function format_lvm()
+{
+        local filesystem disk volumes counter
+        filesystem="${1}"
+        disk="${2}"
+        volumes="${2}"
+        counter=2
+
+        local has_efi has_boot
+        has_efi=0
+        has_boot=0
+
+        for i in "${!volumes[@]}"; do
+                if [[ "${i}" == "/boot" ]]; then
+                        has_boot=1
+                fi
+                if [[ "${i}" == "/efi" ]]; then
+                        has_efi=1
+                fi
+        done
+
+        # if [[ "${has_efi}"
+
+        for i in "${!volumes[@]}"; do
+                if [[ "${i}" == "/efi" ]]; then
+                        mkfs.fat -F 32 "$(blkid -L ESP)"
+                fi
+        done
+
+        for i in "/dev/vg_archlinux/"*; do
+                mkfs."${filesystem}" "/dev/vg_archlinux/${i}"
+        done
+
 }

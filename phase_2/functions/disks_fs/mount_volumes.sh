b31a2bb8ae09c1b489fb770a32fe2888f0ca08f5
2ELCN0168 <eluciani@cpzardaigoro01o.home.arpa>
2025-02-06 17:08:57 +0100

    Added logging

--- phase_2/functions/disks_fs/mount_volumes.sh
+++ phase_2/functions/disks_fs/mount_volumes.sh
@@ -3,35 +3,60 @@ function mount_volumes()
         local volumes_table
         volumes_table="$(blkid -o value -s LABEL)"
 
-        for i in "${volumes_table[@]}"; do
+        # NOTE:
+        # I need the array splitting below.
+
+        for i in ${volumes_table[@]}; do
                 if [[ "${i}" == "root" ]]; then
                         mount "$(blkid -L root)" "/mnt"
+                        mount_log "${i}" "/mnt"
                 fi
         done
 
-        for i in "${volumes_table[@]}"; do
+        for i in ${volumes_table[@]}; do
+                if [[ "${i}" == "boot" ]]; then
+                        mount --mkdir "$(blkid -L boot)" "/mnt/boot"
+                        mount_log "${i}" "/mnt/boot"
+                fi
+
                 if [[ "${i}" == "ESP" ]]; then
-                        mount --mkdir "$(blkid -L ESP)" "/mnt/boot"
+                        mount --mkdir "$(blkid -L ESP)" "/mnt/efi"
+                        mount_log "${i}" "/mnt/efi"
+                        mkdir -p "/mnt/efi/arch"
+                        mount --bind "/mnt/efi/arch" "/mnt/boot"
+                        mount_log "${i}" "/mnt/boot"
                 fi
 
                 if [[ "${i}" == "var" ]]; then
                         mount --mkdir "$(blkid -L var)" "/mnt/var"
+                        mount_log "${i}" "/mnt/var"
                 fi
 
                 if [[ "${i}" == "var_log" ]]; then
                         mount --mkdir "$(blkid -L var_log)" "/mnt/var/log"
+                        mount_log "${i}" "/mnt/var/log"
                 fi
 
                 if [[ "${i}" == "tmp" ]]; then
                         mount --mkdir "$(blkid -L tmp)" "/mnt/tmp"
+                        mount_log "${i}" "/mnt/tmp"
                 fi
 
                 if [[ "${i}" == "usr" ]]; then
                         mount --mkdir "$(blkid -L usr)" "/mnt/usr"
+                        mount_log "${i}" "/mnt/usr"
                 fi
 
                 if [[ "${i}" == "home" ]]; then
                         mount --mkdir "$(blkid -L home)" "/mnt/home"
+                        mount_log "${i}" "/mnt/home"
                 fi
+
+                printf "%b" "\n"
         done
+}
+
+function mount_log()
+{
+        printf "%b" "${INFO} Mounting ${C_P}${1}${N_F} to ${C_P}${2}${N_F}.\n"
 }
